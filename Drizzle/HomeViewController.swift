96b790aa1a6c55a21be56aea252340312fc627b7
Sam Galizia <samgalizia@gmail.com>
2017-04-11 12:54:46 -0400

    Some intial set up for the HomeViewController

---
+++ Drizzle/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Drizzle
+//
+//  Created by Sam Galizia on 4/11/17.
+//  Copyright © 2017 Sam Galizia. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+}
