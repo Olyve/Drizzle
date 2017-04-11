8603fd3f58b0b84a2c300ac25e09486b6bf88130
Sam Galizia <samgalizia@gmail.com>
2017-04-11 15:43:26 -0400

    Added HomeViewController as root view

--- Drizzle/HomeViewController.swift
+++ Drizzle/HomeViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+  fileprivate let viewModel: HomeViewModelType
   
-  override func viewDidLoad() {
+  init(viewModel: HomeViewModelType = HomeViewModel())
+  {
+    self.viewModel = viewModel
+    
+    super.init(nibName: "HomeViewController", bundle: nil)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func viewDidLoad()
+  {
     super.viewDidLoad()
     
+  }
+}
+
+// Helpers
+extension HomeViewController {
+  func checkHomeLocation()
+  {
     
   }
 }
