eb8450f98cc40caed56867ab2d7b2eb70cfeacf9
Sam Galizia <samgalizia@gmail.com>
2017-04-12 14:32:07 -0400

    Added ColorUtilities with app colors as extensions
    Adds the app colors as an extension of UIColor to be used as other
    UIColors. ex. UIColor.drizzleGreen

---
+++ Drizzle/ColorUtilities.swift
@@ -0,0 +1,24 @@
+//
+//  ColorUtilities.swift
+//  Drizzle
+//
+//  Created by Sam Galizia on 4/12/17.
+//  Copyright © 2017 Sam Galizia. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Add app colors to UIColor
+extension UIColor {
+  static var drizzleWhite: UIColor {
+    return UIColor(red: (229/255), green: (209/255), blue: (219/255), alpha: 1)
+  }
+  
+  static var drizzleDarkGray: UIColor {
+    return UIColor(red: (66/255), green: (80/255), blue: (70/255), alpha: 1)
+  }
+  
+  static var drizzleGreen: UIColor {
+    return UIColor(red: (159/255), green: (98/255), blue: (151/255), alpha: 1)
+  }
+}
