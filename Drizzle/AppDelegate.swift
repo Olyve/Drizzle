c9e43f26d5a0efa0e06af6ecf204c89e1766cefa
Sam Galizia <samgalizia@gmail.com>
2017-05-22 15:51:04 -0400

    Added function to determine if running tests

--- Drizzle/AppDelegate.swift
+++ Drizzle/AppDelegate.swift
@@ -61,7 +61,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillTerminate(_ application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
-
-
 }
+
+#if DEBUG
+// Mark: - Determine if Running Tests
+func isRunningUnitTests() -> Bool
+{
+  return ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil
+}
+#endif
 
